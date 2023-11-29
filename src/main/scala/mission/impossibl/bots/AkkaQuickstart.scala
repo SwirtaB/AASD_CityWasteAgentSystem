306b45c8c5b3d32f0b51ddef1205fa85a1cf7116
Joanna Sokołowska <joana.sokolowska@gmail.com>
2023-11-29 17:46:15 +0100

    feat: akka waste source garbage

--- src/main/scala/mission/impossibl/bots/AkkaQuickstart.scala
+++ src/main/scala/mission/impossibl/bots/AkkaQuickstart.scala
@@ -1,57 +1,16 @@
 package mission.impossibl.bots
 
-import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
-import mission.impossibl.bots.GreeterMain.SayHello
+import mission.impossibl.bots.WasteSource.ProduceGarbage
 
-object Greeter {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
-
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    message.replyTo ! Greeted(message.whom, context.self)
-    Behaviors.same
-  }
-}
-
-object GreeterBot {
-
-  def apply(max: Int): Behavior[Greeter.Greeted] = {
-    bot(0, max)
-  }
-
-  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
-    Behaviors.receive { (context, message) =>
-      val n = greetingCounter + 1
-      context.log.info("Greeting {} for {}", n, message.whom)
-      if (n == max) {
-        Behaviors.stopped
-      } else {
-        message.from ! Greeter.Greet(message.whom, context.self)
-        bot(n, max)
-      }
-    }
-}
-object GreeterMain {
-
-  final case class SayHello(name: String)
-
-  def apply(): Behavior[SayHello] =
-    Behaviors.setup { context =>
-      val greeter = context.spawn(Greeter(), "greeter")
-
-      Behaviors.receiveMessage { message =>
-        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
-        greeter ! Greeter.Greet(message.name, replyTo)
-        Behaviors.same
-      }
-    }
-}
+import java.util.Random
+import scala.concurrent.duration.{FiniteDuration, SECONDS}
 
 object AkkaQuickstart extends App {
-  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "AkkaQuickStart")
-  greeterMain ! SayHello("Charles")
+  val random = new Random()
+  val sampleSource: ActorSystem[WasteSource.SourceCommands] = ActorSystem(WasteSource((1,1), null), "AkkaQuickStart")
+
+  implicit val ec = sampleSource.executionContext
+  sampleSource.scheduler.scheduleAtFixedRate(FiniteDuration(1, SECONDS), FiniteDuration(5, SECONDS))(() => sampleSource ! ProduceGarbage(random.nextInt()))
+  sampleSource ! ProduceGarbage(12)
 }
--- src/main/scala/mission/impossibl/bots/WasteSource.scala
+++ src/main/scala/mission/impossibl/bots/WasteSource.scala
@@ -6,32 +6,39 @@ import akka.actor.typed.scaladsl.Behaviors
 
 object WasteSource {
   sealed trait SourceCommands;
-  final case class GarbageCollectionScore(score:Int) extends SourceCommands
+
+  final case class GarbageCollectionScore(score: Int) extends SourceCommands
+
   final case class GetGarbage(maxAmount: Int, garbageCollector: ActorRef) extends SourceCommands
+
   final case class ProduceGarbage(amount: Int) extends SourceCommands
 
   sealed trait SourceResponses
-  final case class SendGarbage(actualAmount:Int, sourceId: Int) extends SourceResponses
+
+  final case class SendGarbage(actualAmount: Int, sourceId: Int) extends SourceResponses
 
 
-  def apply(location:(Int, Int), orchestrator: ActorRef): Behavior[SourceCommands]
-  = source(0, location, 0, orchestrator, 1)
+  def apply(location: (Int, Int), orchestrator: ActorRef): Behavior[SourceCommands] = source(0, location, 0, orchestrator, 1)
 
-  def source(garbage:Int, location:(Int, Int), score: Int, orchestrator: ActorRef, sourceId: Int): Behavior[SourceCommands] =
-    Behaviors.receive(
+
+  def source(garbage: Int, location: (Int, Int), score: Int, orchestrator: ActorRef, sourceId: Int): Behavior[SourceCommands] =
+    Behaviors.receive {
       (context, message) => {
+        context.log.info("Message received {}", message)
         message match {
           case GarbageCollectionScore(newScore) =>
             source(garbage, location, score + newScore / 2, orchestrator, sourceId)
           case GetGarbage(maxAmount, garbageCollector) =>
-            context.log.debug("Garbage received")
+            context.log.info("Garbage received {}", maxAmount)
             val garbageLeft = if (maxAmount >= garbage) 0 else garbage - maxAmount
             garbageCollector ! SendGarbage(garbage - garbageLeft, sourceId)
             source(garbageLeft, location, score, orchestrator, sourceId)
 
-            //simulate garbage production
-          case ProduceGarbage(amount) => source(garbage + amount, location, score, orchestrator, sourceId)
+          //simulate garbage production
+          case ProduceGarbage(amount) =>
+            context.log.info(s"Garbage collected {}", amount)
+            source(garbage + amount, location, score, orchestrator, sourceId)
         }
       }
-    )
+    }
 }
