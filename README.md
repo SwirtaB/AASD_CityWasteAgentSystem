ca8fd799a0f030ae03a09289f5eb76175fa81fc8
Joanna Sokołowska <joana.sokolowska@gmail.com>
2023-11-29 11:20:30 +0100

    feat: akka sample with wasteSource

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AASD_CityWasteAgentSystem
+
+This is a normal sbt project. You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
---
+++ src/main/scala/mission/impossibl/bots/WasteSource.scala
@@ -0,0 +1,37 @@
+package mission.impossibl.bots
+
+import akka.actor.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+
+object WasteSource {
+  sealed trait SourceCommands;
+  final case class GarbageCollectionScore(score:Int) extends SourceCommands
+  final case class GetGarbage(maxAmount: Int, garbageCollector: ActorRef) extends SourceCommands
+  final case class ProduceGarbage(amount: Int) extends SourceCommands
+
+  sealed trait SourceResponses
+  final case class SendGarbage(actualAmount:Int, sourceId: Int) extends SourceResponses
+
+
+  def apply(location:(Int, Int), orchestrator: ActorRef): Behavior[SourceCommands]
+  = source(0, location, 0, orchestrator, 1)
+
+  def source(garbage:Int, location:(Int, Int), score: Int, orchestrator: ActorRef, sourceId: Int): Behavior[SourceCommands] =
+    Behaviors.receive(
+      (context, message) => {
+        message match {
+          case GarbageCollectionScore(newScore) =>
+            source(garbage, location, score + newScore / 2, orchestrator, sourceId)
+          case GetGarbage(maxAmount, garbageCollector) =>
+            context.log.debug("Garbage received")
+            val garbageLeft = if (maxAmount >= garbage) 0 else garbage - maxAmount
+            garbageCollector ! SendGarbage(garbage - garbageLeft, sourceId)
+            source(garbageLeft, location, score, orchestrator, sourceId)
+
+            //simulate garbage production
+          case ProduceGarbage(amount) => source(garbage + amount, location, score, orchestrator, sourceId)
+        }
+      }
+    )
+}
