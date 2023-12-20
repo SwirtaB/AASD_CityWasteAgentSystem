c79f87dcc9c1a394b2863b2da6da31259e531261
Joanna Sokołowska <joana.sokolowska@gmail.com>
2023-12-20 11:48:23 +0100

    refactor: check level as function not message

--- src/main/scala/mission/impossibl/bots/source/WasteSource.scala
+++ src/main/scala/mission/impossibl/bots/source/WasteSource.scala
@@ -1,6 +1,6 @@
 package mission.impossibl.bots.source
 
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 import mission.impossibl.bots.collector.GarbageCollector.CollectGarbage
 import mission.impossibl.bots.orchestrator.GarbageOrchestrator
@@ -9,61 +9,60 @@ import java.util.UUID
 import scala.concurrent.duration._
 
 object WasteSource {
-  private val DisposalPercentFull = 0.7
+  private val DisposalPercentFull    = 0.7
   private val DisposalAuctionTimeout = 3.seconds
-  private val LatenessTolerance = 10.seconds
+  private val LatenessTolerance      = 10.seconds
 
-  def apply(instance: Instance): Behavior[Command] = {
+  def apply(instance: Instance): Behavior[Command] =
     source(instance, State())
-  }
 
   private def source(instance: Instance, state: State): Behavior[Command] =
-    Behaviors.receive {
-      (context, message) => {
-        message match {
-          case CheckGarbageLevel() =>
-            context.log.info("Checking garbage level")
-            if (state.collectionTimeout.isEmpty && state.auctionTimeout.isEmpty && state.garbage > DisposalPercentFull * instance.capacity) {
-              instance.orchestrator ! GarbageOrchestrator.GarbageCollectionRequest(
-                instance.id, instance.location, context.self, state.garbage
-              )
-              val auctionTimeout = context.scheduleOnce(DisposalAuctionTimeout, context.self, AuctionTimeout())
-              source(instance, state.copy(auctionTimeout = Some(auctionTimeout)))
-            } else {
-              Behaviors.same
-            }
-          case ProduceGarbage(amount) => // simulate garbage production
-            context.log.info(
-              s"New garbage in town! {}, current amount: {}",
-              amount, state.garbage + amount
-            )
-            context.self ! CheckGarbageLevel()
-            source(instance, state.copy(state.garbage + amount))
-          case AuctionTimeout() =>
-            context.log.info("Timed out but no auction result")
-            context.self ! CheckGarbageLevel()
-            source(instance, state.copy(auctionTimeout = None))
+    Behaviors.receive { (context, message) =>
+      message match {
+        case ProduceGarbage(amount) => // simulate garbage production
+          context.log.info(
+            s"New garbage in town! {}, current amount: {}",
+            amount,
+            state.garbage + amount
+          )
+          val updatedState = state.copy(state.garbage + amount)
+          source(instance, checkGarbageLevel(updatedState, instance, context))
 
-          case DisposeGarbage(maxAmount, collectorRef) =>
-            val garbageToCollect = maxAmount.max(state.garbage)
-            collectorRef ! CollectGarbage(garbageToCollect)
-            state.collectionTimeout.map(_.cancel())
-            val garbagePassed =state.garbage - garbageToCollect
-            context.log.info("Passing {} garbage", garbagePassed)
-            source(instance, state.copy(garbage = garbagePassed, collectionTimeout = None))
+        case AuctionTimeout() =>
+          context.log.info("Timed out but no auction result")
+          source(instance, checkGarbageLevel(state.copy(auctionTimeout = None), instance, context))
 
-          case GarbageCollectionInfo(collectorId, estimatedArrival) =>
-            context.log.info("Collector {} will arrive in {}", collectorId, estimatedArrival)
-            state.auctionTimeout.map(_.cancel()) //todo maybe one timeout could suffice for both
-            val collectionTimeout = state.collectionTimeout.getOrElse(context.scheduleOnce(estimatedArrival + LatenessTolerance, context.self, CollectionTimeout()))
-            source(instance, state.copy(collectionTimeout = Some(collectionTimeout), estimatedCollectorArrival = Some(estimatedArrival), auctionTimeout = None))
+        case DisposeGarbage(maxAmount, collectorRef) =>
+          val garbageToCollect = maxAmount.max(state.garbage)
+          collectorRef ! CollectGarbage(garbageToCollect)
+          state.collectionTimeout.map(_.cancel())
+          val garbagePassed = state.garbage - garbageToCollect
+          context.log.info("Passing {} garbage", garbagePassed)
+          source(instance, state.copy(garbage = garbagePassed, collectionTimeout = None))
 
-          case CollectionTimeout() =>
-            context.log.info("Collection Timeout")
-            context.self ! CheckGarbageLevel()
-            source(instance, state.copy(collectionTimeout = None))
-        }
+        case GarbageCollectionInfo(collectorId, estimatedArrival) =>
+          context.log.info("Collector {} will arrive in {}", collectorId, estimatedArrival)
+          state.auctionTimeout.map(_.cancel()) // todo maybe one timeout could suffice for both
+          val collectionTimeout = state.collectionTimeout.getOrElse(context.scheduleOnce(estimatedArrival + LatenessTolerance, context.self, CollectionTimeout()))
+          source(instance, state.copy(collectionTimeout = Some(collectionTimeout), estimatedCollectorArrival = Some(estimatedArrival), auctionTimeout = None))
+
+        case CollectionTimeout() =>
+          context.log.info("Collection Timeout")
+          source(instance, checkGarbageLevel(state.copy(collectionTimeout = None), instance, context))
       }
+    }
+
+  private def checkGarbageLevel(state: State, instance: Instance, context: ActorContext[Command]): State =
+    if (state.collectionTimeout.isEmpty && state.auctionTimeout.isEmpty && state.garbage > DisposalPercentFull * instance.capacity) {
+      instance.orchestrator ! GarbageOrchestrator.GarbageCollectionRequest(
+        instance.id,
+        instance.location,
+        context.self,
+        state.garbage
+      )
+      state.copy(auctionTimeout = Some(context.scheduleOnce(DisposalAuctionTimeout, context.self, AuctionTimeout())))
+    } else {
+      state
     }
 
   sealed trait Command
@@ -73,8 +72,6 @@ object WasteSource {
   final case class GarbageCollectionInfo(collectorId: UUID, estimatedArrival: FiniteDuration) extends Command
 
   final case class DisposeGarbage(maxAmount: Int, collectorRef: ActorRef[CollectGarbage]) extends Command
-
-  private final case class CheckGarbageLevel() extends Command
 
   private final case class AuctionTimeout() extends Command
 
